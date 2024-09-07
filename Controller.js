646ece9f8e42e785186a562547ba195fddb2e3c2
FABIO MENINI CARDOSO DOS SANTOS <fabio.mcsantos@senacsp.edu.br>
2024-09-06 21:17:34 -0300

    first commit

---
+++ Controller.js
@@ -0,0 +1,68 @@
+/*// constantes
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+let app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))  // para trabalhar com requisições post
+app.use(bodyParser.json())  // para trabalhar com requisições json
+
+
+// rotas
+app.post('/', (req, res) => {
+  // console.log(`Com o valor de ${req.body.price} você consegue comprar várias coisas!`)
+  console.log(req.body)
+
+  return res.status(200).send({'sucesso': true})
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, (req, res) => {
+  console.log(`Servidor rodando na porta: ${port}`)
+})*/
+
+require('dotenv').config();
+const express = require('express');
+const Stripe = require('stripe');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Configurar Stripe com a chave secreta
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2024-06-20'
+})
+
+// Middlewares
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/payment-intent', async (req, res) =>{
+    try{
+        const { amount } = req.body
+
+        // Criação do PaymentIntent no Stripe
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+        });
+
+        // Retorna o clientSecret ao frontend
+        res.json({
+            clientSecret: paymentIntent.client_secret
+        });
+    } catch (error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.get('/', (req, res) => {
+    res.json('Servidor Stripe ...rodando');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor Stripe ...rodando em http://127.0.0.1:${port}`);
+});
---
+++ views/Checkout.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import { URL_ROOT } from '@env';
+
+export default function Checkout(props) {
+  
+  useEffect( () => {
+    async function sendServer() {
+      console.log('Efetuando requisição...')
+      await fetch(URL_ROOT, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          price: props.route.params.price,
+          address: props.route.params.address,
+        })
+      }).then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json()
+      }).then(data => {
+        console.log(data)
+      }).catch(error => {
+        console.error('There was a problem with the fetch operation: ', error);
+      })
+    }
+    sendServer()
+  }, []);
+  
+  return (
+    <View style={styles.container}>
+      <Text>O valor da corrida é: {props.route.params.price}</Text>
+      <Text>Seu destino é: {props.route.params.address}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+})
